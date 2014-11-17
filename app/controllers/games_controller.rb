e2403fb1f55b6878b8165d15d7497c5ea7d43373
Chris Yancey <cyancey1@gmail.com>
2014-11-16 17:19:58 -0800

    set default timezone for app to pacific time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,5 @@ class GamesController < ApplicationController
     @nba_games = @games.select {|game| game['sport'] == 'NBA'}.map {|game| NbaGame.new(game)}.select {|game| game.start_date_time.to_date.in_time_zone('Pacific Time (US & Canada)') == @today}
     @east_standings = standings.select{|standing| standing.conference == 'EAST'}.sort_by {|standing| standing.playoff_seed}
     @west_standings = standings.select{|standing| standing.conference == 'WEST'}.sort_by {|standing| standing.playoff_seed}
-
-
   end
 end
