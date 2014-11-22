edbb66ec8b18199a4e035123a9c54ad70aedb98d
Chris Yancey <cyancey1@gmail.com>
2014-11-22 12:58:54 -0800

    added records to standings

--- app/models/nba_standing.rb
+++ app/models/nba_standing.rb
@@ -1,10 +1,13 @@
 class NbaStanding
-  attr_reader :team, :rank, :conference, :games_back, :playoff_seed
+  attr_reader :team, :rank, :conference, :games_back, :playoff_seed, :wins, :losses
   def initialize(args = {})
     @team = "#{args['first_name']} #{args['last_name']}"
     @rank = args['rank']
     @conference = args['conference']
     @games_back = args['games_back']
     @playoff_seed = args['playoff_seed']
+    @wins = args['won']
+    @losses = args['lost']
   end
+
 end
